cc385502097318112d45bce839f147f7eee7998b
Rafaela Rodrigues <rafaelaricorodrigues@gmail.com>
2019-04-29 10:16:38 +0100

    First version of agregator

--- src/ag.c
+++ src/ag.c
@@ -1,11 +1,51 @@
-/* Agregador
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "venda.h"
 
-O programa agregador funciona como filtro.
-Recebe pelo stdin entradas no formato do ficheiro de vendas,
-até end-of-file.
-Nessa altura produz para o stdout os dados agregados de cada artigo com
-vendas efectuadas, contendo o código do artigo,
-a quantidade total e o montante total de vendas do artigo respectivo
-(mantendo o formato do ficheiro de vendas).
+Venda vendas[1024];
+int in[1024];
 
-*/
+
+void atualiza(char* argus[]){
+  int cod=atoi(argus[0]);
+  int quant= atoi(argus[1]);
+  double preco=atof(argus[2]);
+
+  if(in[cod]==0){
+    in[cod]=1;
+    vendas[cod]=new_venda(quant,preco);
+  }
+  else{
+    addQuantidade(vendas[cod],quant);
+    addPreco(vendas[cod],preco);
+
+  }
+}
+
+void separa(char * line, char* argus[]){
+  char* token;
+
+  token=strtok(line," ");
+  for(int i=0;token!=NULL;i++){
+    argus[i]=token;
+    token = strtok(NULL," ");
+  }
+}
+
+void tratamento(char *line){
+
+  char* argus[2];
+  separa(line,argus);
+  atualiza(argus);
+}
+
+
+int main(int argc, char const *argv[]) {
+  for(int i=0;i<1024;i++){
+    in[i]=0;
+  }
+  char* line1=strdup(argv[1]);
+  tratamento(line1);
+  return 0;
+}
